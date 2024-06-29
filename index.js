af39892337ca94529e00e3c1967e62f37e4e0ba3
ThomasSteenAndersen <coolfatboy02@gmail.com>
2024-06-29 12:52:48 +0200

    Trying things

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve all files in the current directory
-app.use(express.static(path.join(__dirname, '/')));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+});
+
+app.get('*', (req, res) => {
+  console.log('GET /' + req.path);
+  res.sendFile(path.join(__dirname.split('\\publish')[0], req.path));
 });
